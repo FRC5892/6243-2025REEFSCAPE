8eb6a3725c3c7662c657d3fb1a2cc4bf86869eee
Krope(Christian) <156374345+Kr0pe@users.noreply.github.com>
2025-04-05 07:57:23 -0500

    Pre-Finals Commit

--- src/main/java/frc/robot/Commands/Autos.java
+++ src/main/java/frc/robot/Commands/Autos.java
@@ -9,8 +9,7 @@ import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public final class Autos {
-  
- public static Command TestCenterTwoPiece(DriveSubsystem drive,CoralSubsystem coral) {
+ public static Command CenterAutoTwoPiece(DriveSubsystem drive,CoralSubsystem coral) {
     // this is like a joystick. But, the speed is always 1 (100% forward
     // and the rotation is always 0 (forward)
     return drive.driveCommand(()->-0.6, ()->0).withTimeout(5)
@@ -45,71 +44,6 @@ public final class Autos {
       // outtake for 5 seconds 
       coral.coralForwardCommand().withTimeout(3));
   }   
-
-  public static Command OneLeftL1Auto(DriveSubsystem drive,CoralSubsystem coral) {
-    // this is like a joystick. But, the speed is always 1 (100% forward
-    // and the rotation is always 0 (forward)
-    return drive.driveCommand(()->-0.6, ()->0).withTimeout(5)
-    .andThen(
-      // outtake for 5 seconds 
-      drive.driveCommand(()->0.6,()->1)
-    )
-    .andThen(
-      drive.driveCommand(()->-0.6, ()->0).withTimeout(2)
-    )
-    .andThen(
-      coral.coralForwardCommand().withTimeout(5)
-    ) ; 
-  } 
-
-  public static Command OneRightL1Auto(DriveSubsystem drive,CoralSubsystem coral) {
-    // this is like a joystick. But, the speed is always 1 (100% forward
-    // and the rotation is always 0 (forward)
-    return drive.driveCommand(()->-0.6, ()->0).withTimeout(5)
-    .andThen(
-      // outtake for 5 seconds 
-      drive.driveCommand(()->0,()->-30)
-    )
-    .andThen(
-      drive.driveCommand(()->-0.6, ()->0).withTimeout(2)
-    )
-    .andThen(
-      coral.coralForwardCommand().withTimeout(5)
-    ); 
-  } 
-  
-  
-
-  public static Command RightOneHalfAuto(DriveSubsystem drive,CoralSubsystem coral) {
-    // this is like a joystick. But, the speed is always 1 (100% forward
-    // and the rotation is always 0 (forward)
-    return drive.driveCommand(()->-0.6, ()->0).withTimeout(5)
-    .andThen(
-      // outtake for 5 seconds 
-      drive.driveCommand(()->0,()->0.30)
-    )
-    .andThen(
-      drive.driveCommand(()->-0.6, ()->0).withTimeout(2)
-    )
-    .andThen(
-      coral.coralForwardCommand().withTimeout(5)
-    )
-    .andThen(
-      drive.driveCommand(()->0.6,()->0).withTimeout(2)
-    )
-    .andThen(
-      drive.driveCommand(()->0,()->0.90).withTimeout(2)
-    )
-    .andThen(
-      drive.driveCommand(()->-0.8, ()->0).withTimeout(2)
-    )
-    .andThen(
-      drive.driveCommand(()->0,()->0.45).withTimeout(2)
-    )
-    .andThen(
-      drive.driveCommand(()->-0.6, ()->0).withTimeout(0)
-    );
-  } 
 
   private Autos() {                                                                                                                                                                                                                                                                                                          
     throw new UnsupportedOperationException("This is a utility class!");
